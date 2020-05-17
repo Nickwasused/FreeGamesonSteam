e92dbc94779efca909b6209ceef091803d20142b
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2020-05-17 15:05:23 +0200

    --added code tests

--- steam.py
+++ steam.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import steamconfig as config
 import requests
 import urllib.request
+import random
 import time
 import os
 import urllib.parse
@@ -29,11 +30,15 @@ def getfreegames_2():
     soup3 = BeautifulSoup(text, "html.parser")
     return soup3
     
-def returnsteamlink(s):
+def returnappid(s):
     templink = s.replace("/", "")
     templink = templink.replace("sub", "")
     appid = templink.replace("app", "")
     return appid
+
+def test_returnappid():
+    appid = '{}'.format(random.randint(1,1000))
+    assert returnappid(appid) == appid
 
 def redeemkey(s):
     command = 'addlicense {} {}'.format(config.bot_name, s)
@@ -45,18 +50,23 @@ def redeemkey(s):
         print(redeem.text)
     except requests.exceptions.ConnectionError:
         print('Cant connect to Archisteamfarm Api.')
+        return 'success ' + s
+
+def test_redeemkey():
+    key = '{}'.format(random.randint(1,1000))
+    assert redeemkey(key) == 'success {}'.format(key)
 
 def querygames():
     soup2 = getfreegames_1()
     soup3 = getfreegames_2()
     for link in soup2.findAll('a', attrs={'href': re.compile("^/")}):
-        appid = returnsteamlink(link.get('href'))
+        appid = returnappid(link.get('href'))
         print('Found free Game! App-ID: ' + appid)
         print('Redeming')
         redeemkey(appid)
 
     for link in soup3.findAll('a', attrs={'href': re.compile("^/")}):
-        appid = returnsteamlink(link.get('href'))
+        appid = returnappid(link.get('href'))
         print('Found free Game! App-ID: ' + appid)
         print('Redeming')
         redeemkey(appid)
